db497f8c91e37692e44daf2a247315b8704aec08
Dorothy Rifai <drifai28@gmail.com>
2020-12-28 12:04:11 -0500

    fix AllocationServiceImpl when QOH is 0 sfg issue # 18

--- src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
+++ src/main/java/guru/sfg/beer/inventory/service/services/AllocationServiceImpl.java
@@ -56,11 +56,16 @@ public class AllocationServiceImpl implements AllocationService {
 		    	inventory = inventory - qtyToAllocate;
 		    	beerOrderLine.setQuantityAllocated(orderQty);
 		    	beerInventory.setQuantityOnHand(inventory);
+		    	beerInventoryRepository.save(beerInventory);
 		    } else if (inventory > 0) {
 		    	// partial allocation
 		    	beerOrderLine.setQuantityAllocated(allocatedQty + inventory);
 		    	beerInventory.setQuantityOnHand(0);
 		    	
+		    	
+		    }
+		    
+		    if(beerInventory.getQuantityOnHand() == 0) {
 		    	beerInventoryRepository.delete(beerInventory);
 		    }
 		});
